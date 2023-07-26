bbd93c33a4df2cd8705774754423516f89eb5ab4
punyajoy <punyajoysaha1998@gmail.com>
2023-07-26 17:36:20 -0400

    moved bash scripts

---
+++ bash_scripts/bash_loop-gpu1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# python Rationale_extractor_training.py Params/all_params_bert_toxic_rationale.json 24 1
+# python Rationale_extractor_training.py Params/all_params_bert_toxic_rationale.json 8 1
+# python Rationale_extractor_training.py Params/all_params_bert_toxic_rationale_target.json 96 1
+# python Rationale_extractor_training.py Params/all_params_bert_toxic_rationale_target.json 9 1
+
+
+
+# python Manual_train_caller-all-but-one.py Davidson 1
+# python Manual_train_caller-all-but-one.py Waseem 1
+# python Manual_train_caller-all-but-one.py Basile 1
+# python Manual_train_caller-all-but-one.py Olid 1
+# python Manual_train_caller-all-but-one.py Founta 1
---
+++ bash_scripts/bash_test_pred.sh
@@ -0,0 +1,5 @@
+python testing_with_predictions.py -dataset_name Davidson -model_name Transform_Rationale_SelfAttn_Drpt_corrected -random_seed 2021 -training_points 50
+python testing_with_predictions.py -dataset_name Founta -model_name Transform_Rationale_SelfAttn_Drpt_corrected -random_seed 2021 -training_points 50
+python testing_with_predictions.py -dataset_name Basile -model_name Transform_Rationale_SelfAttn_Drpt_corrected -random_seed 2021 -training_points 50
+python testing_with_predictions.py -dataset_name Olid -model_name Transform_Rationale_SelfAttn_Drpt_corrected -random_seed 2021 -training_points 50
+python testing_with_predictions.py -dataset_name Waseem -model_name Transform_Rationale_SelfAttn_Drpt_corrected -random_seed 2021 -training_points 50
